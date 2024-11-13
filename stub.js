7846b0b96bb23759b77f580e195fe1f32c64e4d2
Bernhard Gailer <113438267+bgailer@users.noreply.github.com>
2024-11-13 09:21:40 +0100

    Create stub.js

---
+++ stub.js
@@ -0,0 +1,7 @@
+var stack_js = {
+get_content: (id) => new Promise((res) => res(document.getElementById(id))),
+toggle_visibility: (id) => document.getElementById(id).style.display = "block",
+request_access_to_input: (id, bool) => new Promise((res) => res(id)),
+switch_content: (id, newcontent) => document.getElementById(id).innerHTML = newcontent,
+register_external_button_listener: (btnid, cb) => document.getElementById(btnid).addEventListener("click", (event) => cb())
+};
