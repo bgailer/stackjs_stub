886ef04ba3383e764dc2a4c86443ba16e16cc676
Bernhard Gailer <113438267+bgailer@users.noreply.github.com>
2024-11-13 09:23:27 +0100

    Update stub.js

--- stub.js
+++ stub.js
@@ -5,3 +5,4 @@ request_access_to_input: (id, bool) => new Promise((res) => res(id)),
 switch_content: (id, newcontent) => document.getElementById(id).innerHTML = newcontent,
 register_external_button_listener: (btnid, cb) => document.getElementById(btnid).addEventListener("click", (event) => cb())
 };
+export default stack_js;
